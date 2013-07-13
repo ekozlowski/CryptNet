54591669c31e8401505d80490ddaee2a74069c33
Bryan Wyatt <brwyatt@gmail.com>
2013-07-12 22:47:48 -0500

    This should be a decent start
    Just some core classes/interfaces/superclasses

---
+++ src/com/junglecatsoftware/cryptnet/network/ConnectionManager.java
@@ -0,0 +1,39 @@
+/* 
+ * Copyright 2012 Bryan Wyatt
+ * 
+ * This file is part of CryptNet.
+ *  
+ * CryptNet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CryptNet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CryptNet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.junglecatsoftware.cryptnet.network;
+
+import com.junglecatsoftware.cryptnet.messaging.Messagable;
+import com.junglecatsoftware.cryptnet.messaging.Message;
+
+public class ConnectionManager implements Messagable {
+
+	@Override
+	public void inboundMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void outboundMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
