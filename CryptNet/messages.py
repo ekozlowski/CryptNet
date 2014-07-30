aeb184a7c3f998d6ca45670b64b2d432b9a7fc9e
Bryan Wyatt <brwyatt@gmail.com>
2014-07-29 23:06:08 -0500

    Rewrote Message for custom serialization
    This will improve network IO overhead and allow implementations in other
    languages. Additionally includes new Payload class (which should be used as a
    superclass) and is used for serialization.

--- CryptNet/messages.py
+++ CryptNet/messages.py
@@ -13,43 +13,77 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import uuid, pickle, time
+import uuid
+from datetime import datetime
+from calendar import timegm
+from hashlib import sha512
+
+def int_to_bytearray(integer, length=4):
+    b = bytearray(length)
+    i = length-1
+    while integer > 0 and i >= 0:
+        b[i]=integer%256
+        i -= 1
+        integer = int(integer/256)
+    return b
+
+
+class Payload:
+    """CryptNet Message Payload template"""
+
+    payloadString = 'Default Message'
+
+    def serialize(self):
+        return bytearray(self.payloadString, 'utf-8')
+
+    @staticmethod
+    def deserialize(rawString):
+        p = Payload()
+        p.payloadString = rawString
+        return p
+
 
 class Message:
-    """CryptNet Message"""
+    """CryptNet Message
     
-    def __init__(self, msgClass=0, msgType=0, dest=None, src=None, content=None):
-        self.msgUuid = str(uuid.uuid4())
-        self.time = time.time()
-        self.msgClass = msgClass
-        self.msgType = msgType
-        self.dest = dest
-        self.src = src
-        self.content = content
-
-    def encryptMessage(self):
-        """Creates CryptNet EncryptedMessage based on this Message instance"""
-        print("Not Implemented")
+    Format: [16Bytes:msgUuid][8Bytes:timestamp][1Bytes:msgPluginIdLength][xBytes:msgPluginId][64Bytes:recipient][encrypted:[64Bytes:sender][4Bytes:payloadLength][xBytes:payload][64Bytes:signedPayloadHash]]
+    """
     
-    def serialize(self):
-        return pickle.dumps(self)
-
-class EncryptedMessage
-    """CryptNet EncryptedMessage"""
-
-    def __init__(self, msgClass=0, msgType=0, dest=None, msgUuid=None, payload=None):
-        self.msgUuid = msgUuid
-        self.msgClass = msgClass
-        self.msgType = msgType
-        self.dest = dest
+    def __init__(self, recipient, sender, pluginId=None, payload=None):
+        if pluginId is None:
+            pluginId = ''
+        elif pluginId.__len__() >= 256:
+            raise TypeError('pluginId cannot be longer than 255 characters')
+        if payload is None:
+            payload = Payload()
+        self.msgUuid = uuid.uuid4()
+        self.time = datetime.utcnow()
+        self.recipient = recipient
+        self.sender = sender
+        self.pluginId = pluginId
         self.payload = payload
 
-    def decryptMessage(self):
-        """Creates CryptNet Message based on this EncryptedMessage instance"""
-        print("Not Implemented")
+    def to_bytes(self):
+        b = bytearray()
+        b += self.msgUuid.bytes
+        b += int_to_bytearray(timegm(self.time.utctimetuple()),8)
+        b += bytes([self.pluginId.__len__()])
+        b += bytearray(self.pluginId, 'utf-8')
+        b += self.recipient
 
-    def serialize(self):
-        return pickle.dumps(self)
+        p = bytearray()
+        p += self.sender
+        pBytes = self.payload.serialize()
+        p += int_to_bytearray(pBytes.__len__(),4)
+        p += pBytes
+        p += sha512(pBytes).digest() # Add Signing
 
-def deserialize(pickleBytes):
-    return pickle.loads(pickleBytes)
+        # Encrypt P
+        b += p
+
+        return b
+
+    @staticmethod
+    def from_bytes(byteArray):
+        raise NotImplementedError('Convert Bytes to Message Object')
+
